0953c33e978e7339faeeda738494951aaf2fbd28
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2022-10-27 19:50:35 +0200

    css added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   root "lists#index"
   resources :lists do
     resources :bookmarks, only: [:create, :new]
+    # Inside -> You need ID
   end
   resources :bookmarks, only: :destroy
+  # Outisde -> Index show edit update destroy
 end
+
+
+# -> edit bookmark, I might understand parent and children relation
