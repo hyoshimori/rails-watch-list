a4cfe7c295a429b952c1f6fdabb86887b10d8328
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2022-10-31 01:04:43 +0100

    work in progress

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  validates :comment, length: { minimum: 6 }
+  validates :comment, length: { minimum: 6, message: "Heyy too short!" }
   validates :movie_id, uniqueness: { scope: :list_id, message: "It is already in the list" }
 end
